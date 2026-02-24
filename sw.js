f1f74fc2960886ad5c281aad3971a8cfd4b0cbcb
Emiliano Balderas Ramírez <ebalderas@live.com.mx>
2026-02-24 15:41:27 -0600

    v2.2

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 /* sw.js */
-const CACHE_NAME = 'molarprep-v4'; // Cambiado de v1 a v2
+const CACHE_NAME = 'molarprep-v5';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './icon-maskable-192.png',
+  './icon-maskable-512.png'
 ];
 
 self.addEventListener('install', (e) => {
